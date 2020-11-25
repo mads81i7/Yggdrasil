5b235c593a215a51dd0d96597a2b1bc022c53772
katr4 <katr4722@edu.easj.dk>
2020-11-25 12:49:57 +0100

    Starting the order system

--- Yggdrasil/Startup.cs
+++ Yggdrasil/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Yggdrasil.Interfaces;
+using Yggdrasil.Repositories;
 
 namespace Yggdrasil
 {
@@ -24,6 +26,7 @@ namespace Yggdrasil
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<IOrderRepository, OrderRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
