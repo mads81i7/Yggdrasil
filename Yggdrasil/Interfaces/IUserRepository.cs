7ba64d78edacf05ab20f4a77cdc7e3c4c208dcc7
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2020-11-25 11:07:46 +0100

    2020-NOV-25 11:07

---
+++ Yggdrasil/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yggdrasil.Models;
+
+namespace Yggdrasil.Interfaces
+{
+    public interface IUserRepository
+    {
+        public string GetUserForeName(int id);
+        List<User> GetAllUsers();
+    }
+}
