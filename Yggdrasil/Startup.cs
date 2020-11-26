11f98a700c6dfe3fbeb42ba0cc1bc412e462d94f
katr4 <katr4722@edu.easj.dk>
2020-11-26 10:02:56 +0100

    from merge

--- Yggdrasil/Startup.cs
+++ Yggdrasil/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Yggdrasil.Interfaces;
-using Yggdrasil.Repositories;
+using Yggdrasil.Services;
 
 
 namespace Yggdrasil
