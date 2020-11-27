9e4da59d268c6d031df81c69af3be566948eff7a
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2020-11-27 11:05:35 +0100

    2020-NOV-27 11:05

---
+++ Yggdrasil/Helpers/JsonFileWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Yggdrasil.Models;
+
+namespace Yggdrasil
+{
+    public class JsonFileWriter
+    {
+        public static void WriteToJsonCustomer(List<Customer> customers, string JsonFileName)
+        {
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(customers, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(JsonFileName, output);
+        }
+
+        public static void WriteToJsonCourier(List<Courier> couriers, string JsonFileName)
+        {
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(couriers, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(JsonFileName, output);
+        }
+    }
+}
--- Yggdrasil/Models/User.cs
+++ Yggdrasil/Models/User.cs
@@ -8,22 +8,14 @@ namespace Yggdrasil.Models
 {
     public class User
     {
-        [Required]
-        public int Id { get; set; }
-        [Required]
-        public string ForeName { get; set; }
-        [Required]
-        public string SurName { get; set; }
-        [Required]
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string AddressLine1 { get; set; }
         public string AddressLine2 { get; set; }
-        [Required]
         public string City { get; set; }
-        [Required]
         public int PostalCode { get; set; }
-        [Required]
         public string PhoneNo { get; set; }
-        [Required]
         public string EMail { get; set; }
     }
 }
