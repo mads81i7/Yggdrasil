9193cd22232a8a678c8c80cbd38964db645963b8
Mikkel Vestergaard-Jensen <mikk23f8@edu.easj.dk>
2020-11-27 12:56:30 +0100

    More vareoversigt ;_;

--- Yggdrasil/Catalogs/WareCatalog.cs
+++ Yggdrasil/Catalogs/WareCatalog.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Yggdrasil.Models;
+using Yggdrasil.Pages.Wares;
 
 namespace Yggdrasil.Catalogs
 {
     public class WareCatalog
     {
-        private List<Ware> wares { get; }
+        private  List<Ware> wares { get; }
 
         public WareCatalog()
         {
             wares = new List<Ware>();
+            wares.Add(new Ware() {Id = 0, Name = "Pizza", Description = "Test", IsOrganic = true, IsVegan = false, Price = 20, Type = WareType.Fresh});
+            wares.Add(new Ware() {Id = 1, Name = "Baked Beans", IsVegan = true, Price = 15, Type = WareType.Canned, Description = "A tin of beans", IsOrganic = false});
 
+        }
+
+        public List<Ware> AllWares()
+        {
+            return wares;
+        }
+
+        private int WareId = 2;
+        public void AddWare(Ware ware)
+        {
+            wares.Add(ware);
+            ware.Id = WareId;
+            WareId++;
         }
     }
 }
--- Yggdrasil/Pages/Wares/GetAllWares.cshtml.cs
+++ Yggdrasil/Pages/Wares/GetAllWares.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Yggdrasil.Catalogs;
+using Yggdrasil.Models;
 
 namespace Yggdrasil.Pages.Wares
 {
     public class GetAllWaresModel : PageModel
     {
-        public void OnGet()
+        private WareCatalog catalog;
+        public GetAllWaresModel()
         {
+            catalog = new WareCatalog();
+        }
+        public List<Ware> Wares { get; private set; }
+        public IActionResult OnGet()
+        {
+            Wares = catalog.AllWares();
+            return Page();
         }
     }
 }
