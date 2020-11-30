befd85f80189d68bbc54793e4f5329abb6d2e777
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2020-11-30 14:26:31 +0100

    Add project files.

---
+++ Helpers/JsonFileWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Yggdrasil.Models;
+
+namespace Yggdrasil
+{
+    public class JsonFileWriter
+    {
+        public static void WriteToJsonUser(List<User> users, string JsonFileName)
+        {
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(users, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(JsonFileName, output);
+        }
+    }
+}
