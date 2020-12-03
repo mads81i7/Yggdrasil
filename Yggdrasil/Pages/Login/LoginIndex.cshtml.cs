22d7e6934e15387de3d7d97e997ea17b19012bb9
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2020-12-03 10:44:40 +0100

    Login changes / Profile

--- Yggdrasil/Pages/Login/LoginIndex.cshtml.cs
+++ Yggdrasil/Pages/Login/LoginIndex.cshtml.cs
@@ -42,7 +42,7 @@ namespace Yggdrasil.Pages.Login
                         return RedirectToPage("/Index");
                     }
 
-                    return RedirectToPage("SignUp");
+                    return Page();
                 }
             }
 
