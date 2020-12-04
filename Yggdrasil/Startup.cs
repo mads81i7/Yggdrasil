47a1b8e277ffa529671ae38d24fb8cff3ac5a66e
katr4 <katr4722@edu.easj.dk>
2020-12-04 10:03:10 +0100

    fjernede using yggdrasil.catalogs

--- Yggdrasil/Startup.cs
+++ Yggdrasil/Startup.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Yggdrasil.Catalogs;
 using Yggdrasil.Interfaces;
 using Yggdrasil.Services;
 
