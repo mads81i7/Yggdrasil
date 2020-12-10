3c9572714db2fcde036b37af73c9b22bee32c6dd
Jonathan Yannick Thomassen <jona97g8@edu.easj.dk>
2020-12-10 15:47:47 +0100

    Besked hvis ordre-listen er tom i RequestIndex

--- Yggdrasil/Pages/Requests/RequestIndex.cshtml.cs
+++ Yggdrasil/Pages/Requests/RequestIndex.cshtml.cs
@@ -12,12 +12,15 @@ namespace Yggdrasil.Pages.Requests
         private readonly IOrderRepository _orderRepository;
         private readonly LoginService _loginService;
         public List<Order> Orders;
+        public bool EmptyRequestList;
 
         public RequestIndexModel(IOrderRepository orderRepository, LoginService loginService)
         {
             _orderRepository = orderRepository;
             _loginService = loginService;
             Orders = _orderRepository.AllOrders();
+
+            EmptyRequestList = true;
         }
 
         public IActionResult OnPostAccept(int id)
