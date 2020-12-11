91da07dd4d83eb03935644e606a033ee17e0b230
katr4 <katr4722@edu.easj.dk>
2020-12-11 10:15:03 +0100

    start af rabatkode og tab i admin side

---
+++ Yggdrasil/Models/Offer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yggdrasil.Models
+{
+    public class Offer
+    {
+        public double Discount { get; set; }
+        public int Code { get; set; }
+        public string Detail { get; set; }
+    }
+}
---
+++ Yggdrasil/Services/OfferRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Yggdrasil.Models;
+
+namespace Yggdrasil.Services
+{
+    public class OfferRepository
+    {
+        public List<Offer> _Offers { get; set; }
+
+        public List<Offer> AllOffers()
+        {
+            return _Offers;
+        }
+
+        public void AddOffer(Offer offer)
+        {
+            _Offers.Add(offer);
+        }
+
+        public void RemoveOffer()
+        {
+
+        }
+    }
+}
